3197c7a9974f8cbac2b388a16780aa90774ec82d
Vladimir I. Polyakov <order1987@yandex-team.ru>
2017-03-20 11:13:01 +0300

    finally fully working test

--- test.spec.music-666.js
+++ test.spec.music-666.js
@@ -1,25 +1,32 @@
 'use strict';
-
+ 
 const assert = require('assert');
 const webdriver = require('webdriverio'); 
-
-describe('music-666: у альбома есть название', function () {
+ 
+describe('music-666: slider title is visible', function () {
     it('should show title', function(){
         const browser = webdriver.remote({
             desiredCapabilities: {
                 browserName: 'firefox'
             }
         });
-
+ 
         return browser
             .init()
-            .url('https://music.yandex.ru/album/2974326')
-            .waitForVisible('.page-album__title')
-            .getCssProperty('.page-album__title', 'color').then(res => {
-                assert(res.parsed.hex, '000', 'Color is not black: ${res.parsed.hex}');
+            .url('https://music.yandex.ru/home')
+            .waitForVisible('.playlist-slider__title')
+            .getCssProperty('.playlist-slider__title', 'color').then(res => {
+                assert(res.parsed.hex == '#000000', 'Color is not black: ${res.parsed.hex}');
+                console.log(res.parsed.hex);
             })
-            .getCssProperty('.page-album__title', 'font-size').then(res => {
-                assert(res.parsed.value < 36, 'Font-size is small: ${res.parsed.value}');
+
+            .getCssProperty('.playlist-slider__title', 'font-weight').then(res => {
+                assert(res.parsed.value == '700', 'font-weight is not bold: ${res.parsed.value}');
+                console.log(res.parsed.value);
+
+            })
+            .getCssProperty('.playlist-slider__title', 'font-size').then(res => {
+                assert(res.parsed.value == 25, 'Font-size is small: ${res.parsed.value}');
             });
     });
 });
